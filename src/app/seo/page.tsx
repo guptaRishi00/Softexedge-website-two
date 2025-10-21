792ddf175afa66496c69bb61c36f2bdb72b44d5b
guptaRishi00 <rishigupta54066@gmail.com>
2025-10-21 17:24:03 +0530

    added skeleton

---
+++ src/app/seo/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Seo({}: Props) {
+  return <div>Seo</div>;
+}
---
+++ src/app/social/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Social({}: Props) {
+  return <div>Social</div>;
+}
