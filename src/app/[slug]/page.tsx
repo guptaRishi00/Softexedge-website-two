5e2dc8079926e7682e619663f906ee756225792b
lakshay15699 <blakshay66@gmail.com>
2025-10-29 23:57:06 +0530

    updated files with fixed errors

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+// app/[slug]/page.tsx
+
 import { getPageData } from "@/data/loader";
 import HeroSection from "@/components/aboutpage/HeroSectionAbout";
 import OurStory from "@/components/aboutpage/OurStory";
@@ -5,11 +7,15 @@ import OurMission from "@/components/aboutpage/OurMission";
 import WhyChoose from "@/components/aboutpage/WhyChoose";
 import OurTeam from "@/components/aboutpage/OurTeam";
 import Review from "@/components/aboutpage/Review";
-import WhatWeDo from "@/components/homepage/WhatWeDo";
 
-export default async function DynamicPage({ params }: { params: { slug: string } }) {
-  // Fetch page data from Strapi based on slug
-  const response = await getPageData(params.slug);
+export default async function Page({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = params;
+
+  const response = await getPageData(slug);
   const page = response?.data?.[0];
 
   if (!page) {
@@ -24,25 +30,16 @@ export default async function DynamicPage({ params }: { params: { slug: string }
         switch (block.__component) {
           case "shared-components.hero-section":
             return <HeroSection key={index} data={block} />;
-
           case "aboutpage.our-story":
             return <OurStory key={index} data={block} />;
-
           case "aboutpage.our-mission":
             return <OurMission key={index} data={block} />;
-
-          case "homepage.what-we-do":
-            return <WhatWeDo key={index} data={block} />;
-
           case "aboutpage.why-choose":
             return <WhyChoose key={index} data={block} />;
-
           case "aboutpage.our-team":
             return <OurTeam key={index} data={block} />;
-
           case "aboutpage.review":
             return <Review key={index} data={block} />;
-
           default:
             return null;
         }
